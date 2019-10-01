295c037bfdd4ff0029ea2b35c6cdef67df3811b2
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-01 09:42:56 -0400

    initial commit - setting up navigation events, styles

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+const navigation = document.querySelectorAll('.nav .nav-Link');
+
+navigation.forEach(navigate => {
+    navigate.addEventListener('mouseover', event => {
+        event.target.style.color = 'red';
+    });
+
+    navigate.addEventListener('click', event => {
+        event.target.style.color = 'blue';
+        event.stopPropagation();
+    })
+})
